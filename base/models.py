cf61d0a615c58be790677c6a0a19a27d4c06c6d4
Saitore973 <saitorevg@gmail.com>
2022-04-09 12:34:40 +0300

    add project model and define a one to many relationship

--- base/models.py
+++ base/models.py
@@ -9,3 +9,12 @@ class Profile(models.Model):
      bio = models.TextField(max_length=200, blank=True)
      updated = models.DateTimeField(auto_now=True)
      created = models.DateTimeField(auto_now_add=True)
+
+class Project(models.Model):
+    image = CloudinaryField('image')
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user',null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile',null=True)
