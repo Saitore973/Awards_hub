1229716e261780e8943143b8ea48820e28274f36
Saitore973 <saitorevg@gmail.com>
2022-04-09 15:07:56 +0300

    display model data

--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from base.models import Project
+
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def welcome(request):
-    return render(request, 'index.html')
+    projects = Project.objects.all()  
+    return render(request, 'index.html', {"projects": projects})
