ce55f1ddfc9be10b93e7532769849a5413c4e46a
Saitore973 <saitorevg@gmail.com>
2022-04-09 07:02:00 +0300

    add registration

--- awards/urls.py
+++ awards/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls',)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
