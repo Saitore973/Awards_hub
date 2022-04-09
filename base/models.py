f0554fc0c5aae2936c72c25782ee3d2189942978
Saitore973 <saitorevg@gmail.com>
2022-04-09 14:15:20 +0300

    add profile methods

--- base/models.py
+++ base/models.py
@@ -10,6 +10,27 @@ class Profile(models.Model):
      updated = models.DateTimeField(auto_now=True)
      created = models.DateTimeField(auto_now_add=True)
 
+
+     def __str__(self):
+        return str(self.user)
+    
+     def num_image(self):
+        return self.profile.all().count()
+    
+    
+        
+     def save_profile(self):
+        self.save()
+
+     def delete_profile(self):
+        self.delete()
+
+     def update(self, profilePhoto, bio):
+        self.bio = bio
+        self.profilePhoto = profilePhoto
+        self.save()
+
+
 class Project(models.Model):
     image = CloudinaryField('image')
     title = models.CharField(max_length=20)
